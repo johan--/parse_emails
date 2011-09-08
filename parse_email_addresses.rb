b92fd9938dbe4dccf72bc505b93cf532408a224a
Jacob-Kroeze <jlkroeze@gmail.com>
2011-09-07 22:56:36 -0700

    comming along

--- parse_email_addresses.rb
+++ parse_email_addresses.rb
@@ -8,60 +8,23 @@ require 'thin'
 
 Mongoid.database = Mongo::Connection.new('localhost','27017').db('parse_emails')
 
-#@url_array = ["http://www.jcsana.org/", 
-#	"http://www.dorot.org/dfi",
-#	 "http://www.jewish-studies.com/Jewish_Studies_at_Universities/USA/",
-#	 "http://www.google.com/search?q=Jewish+Studies+Departments&ie=utf-8&oe=utf-8&aq=t&rls=org.mozilla:en-US:official&client=firefox-a#sclient=psy&hl=en&client=firefox-a&hs=Le0&rls=org.mozilla:en-US%3Aofficial&source=hp&q=*+site:+jewishstudies*.edu&pbx=1&oq=*+site:+jewishstudies*.edu&aq=f&aqi=&aql=1&gs_sm=e&gs_upl=578l578l3l965l1l1l0l0l0l0l229l431l2-2l2l0&fp=1&biw=1277&bih=844&bav=on.2,or.r_gc.r_pw.r_cp.&cad=b",
-#	"http://www.google.com/search?q=year+programs+in+israel&ie=utf-8&oe=utf-8&aq=t&rls=org.mozilla:en-US:official&client=firefox-a#sclient=psy&hl=en&client=firefox-a&rls=org.mozilla:en-US%3Aofficial&source=hp&q=year+in+israel+site:*.edu&pbx=1&oq=year+in+israel+site:*.edu&aq=f&aqi=&aql=1&gs_sm=e&gs_upl=197788l204318l0l204735l26l20l5l0l0l3l368l4025l0.2.9.4l15l0&fp=1&biw=1277&bih=844&bav=on.2,or.r_gc.r_pw.r_cp.&cad=b",
-#	"http://www.teachforamerica.org/",
-#	"http://blogs.rj.org/reform/",
-#	"http://www.natanet.org/",
-#	"http://www.jewishcamp.org/",
-#	"http://www.americorps.gov/"
-#	]
+@url_array = ["http://www.jcsana.org/", 
+	"http://www.dorot.org/dfi",
+	 "http://www.jewish-studies.com/Jewish_Studies_at_Universities/USA/",
+	"http://www.teachforamerica.org/",
+	"http://blogs.rj.org/reform/",
+	"http://www.natanet.org/",
+	"http://www.jewishcamp.org/",
+	"http://www.americorps.gov/"
+	]
 	 
-@url_array = ["http://www.jcsana.org/",
-             "http://www.jewish-studies.com/Jewish_Studies_at_Universities/USA/"]
+#@url_array = ["http://www.jcsana.org/",
+#             "http://www.jewish-studies.com/Jewish_Studies_at_Universities/USA/"]
 class Site
   include Mongoid::Document
   embeds_many :pages
   field :url
-  def self.get_pages(url_array)
-    url = url_array #url_array.each do |url|
-      site = Site.new(:url => url)
-      Anemone.crawl(url) do |anemone|
-        anemone.on_every_page do |page|
-        if page.html?
-          page_title = page.doc.at('title').inner_html rescue nil
-          puts page_title
-          unless page_title == "Board &amp; Staff"#TODO matches "contact" "board" "staff" "faculty
-            page_title = nil
-          end
-
-          #page.doc.xpath('//*[starts-with(*, "President")]').each do |node|
-          #  contacts = node.parent.text
-          #end
-
-          if page_title
-            site.pages << Page.new(url: page.url.to_s, page_title: page_title )
-            puts site.pages.each { |p| puts "found url => \"#{p.url.to_s}\"";
-                                     puts "found title => \"#{page_title}\"";}
-          end
-
-          anemone.after_crawl { site.save }
-        end
-        end
-      end
-    #end 
-  end
- # def self.search_text
- #   node.xpath('.//title[regex(., "\w+")]', Class.new {
- #     def regex node_set, regex
- #       node_set.find_all { |node| node['some_attribute'] =~ /#{regex}/ }
- #     end
- #   }.new)
- # end
-
+  
 end
 
 class Page
@@ -80,6 +43,57 @@ class Contact
   field :content
   field :content_snippet
 end
+
+def test(url)
+  page_search_array = []
+  search_field_input = "board staff", "contact us", "about us", "faculty", "directory", "directory:"
+  page_search_array << search_field_input.join(" ").split
+  page_search_array.flatten!
+
+  site = Site.new(:url => url)
+  puts "created #{site.inspect}"
+  Anemone.crawl(url, :discard_page_bodies => true) do |anemone|
+  #TODO: Save separate sites (how to pop get url from url_array?)
+    anemone.on_every_page do |page|
+      page_title = page.doc.at('title').inner_html.chomp.downcase rescue nil 
+      search_results = page_search_array & page_title.split rescue nil
+      puts page_title
+      if page.html? && search_results && !search_results.empty? 
+        url = url.to_s
+        site.pages << Page.new(:url => page.url.to_s, :page_title => page_title)
+        site.save
+        puts "*************/created******** #{site.pages.last}"
+        page.discard_doc!()
+        Anemone.crawl(@links.to_s, :discard_page_bodies => true, :depth_limit => 4) do |anemone|
+          anemone.on_every_page do |page|
+            page_title = page.doc.at('title').inner_html.chomp.downcase rescue nil 
+            search_results = page_search_array & page_title.split rescue nil
+            puts page_title
+            if page.html? && search_results && !search_results.empty? 
+              url = url.to_s
+              site.pages << Page.new(:url => page.url.to_s, :page_title => page_title)
+              site.save
+            end
+          end
+        end
+        page_title = nil; body = nil; search_results = nil; 
+        page = nil; email = nil; name = nil; content = nil; content_snippet = nil; 
+      end
+    end
+  end
+end
+
+ #page.doc.xpath('//*[starts-with(*, "President")]').each do |node|
+ #  contacts = node.parent.text
+ #end
+
+ # def self.search_text
+ #   node.xpath('.//title[regex(., "\w+")]', Class.new {
+ #     def regex node_set, regex
+ #       node_set.find_all { |node| node['some_attribute'] =~ /#{regex}/ }
+ #     end
+ #   }.new)
+ # end
 
 get '/' do
   "This is a working url : #{Site.first.url}" 
