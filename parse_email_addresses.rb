dd3fbf70700ebdede3ea3827608c6a3d7092764e
Jacob-Kroeze <jlkroeze@gmail.com>
2011-09-12 13:03:20 -0700

    quick commit to merge

--- parse_email_addresses.rb
+++ parse_email_addresses.rb
@@ -20,16 +20,17 @@ Mongoid.database = Mongo::Connection.new('localhost','27017').db('parse_emails')
 	 
 #@url_array = ["http://www.jcsana.org/",
 #             "http://www.jewish-studies.com/Jewish_Studies_at_Universities/USA/"]
-class Site
+class SiteGroup
   include Mongoid::Document
   embeds_many :pages
-  field :url
+  field :urls
+  field :name
   
 end
 
 class Page
   include Mongoid::Document
-  embedded_in :site
+  embedded_in :site_group
   embeds_many :contacts
   field :url
   field :page_title
@@ -44,7 +45,7 @@ class Contact
   field :content_snippet
 end
 
-def test(url)
+def test(urls)
   page_search_array = []
   search_field_input = "board staff", "contact us", "about us", "faculty", "directory", "directory:"
   page_search_array << search_field_input.join(" ").split
@@ -52,30 +53,19 @@ def test(url)
 
   site = Site.new(:url => url)
   puts "created #{site.inspect}"
-  Anemone.crawl(url, :discard_page_bodies => true) do |anemone|
-  #TODO: Save separate sites (how to pop get url from url_array?)
+  Anemone.crawl(urls, :discard_page_bodies => true, :depth_limt => 3) do |anemone|
+    site_group = SiteGroup.new(:urls => urls) #, :name => params[:name] )
+    anemone.storage = Anemone::Storage.MongoDB
     anemone.on_every_page do |page|
       page_title = page.doc.at('title').inner_html.chomp.downcase rescue nil 
       search_results = page_search_array & page_title.split rescue nil
       puts page_title
       if page.html? && search_results && !search_results.empty? 
         url = url.to_s
-        site.pages << Page.new(:url => page.url.to_s, :page_title => page_title)
-        site.save
-        puts "*************/created******** #{site.pages.last}"
+        site_group.pages << Page.new(:url => page.url.to_s, :page_title => page_title)
+        site_group.save
+        puts "*************/created******** #{site_group.pages.last}"
         page.discard_doc!()
-        Anemone.crawl(@links.to_s, :discard_page_bodies => true, :depth_limit => 4) do |anemone|
-          anemone.on_every_page do |page|
-            page_title = page.doc.at('title').inner_html.chomp.downcase rescue nil 
-            search_results = page_search_array & page_title.split rescue nil
-            puts page_title
-            if page.html? && search_results && !search_results.empty? 
-              url = url.to_s
-              site.pages << Page.new(:url => page.url.to_s, :page_title => page_title)
-              site.save
-            end
-          end
-        end
         page_title = nil; body = nil; search_results = nil; 
         page = nil; email = nil; name = nil; content = nil; content_snippet = nil; 
       end
