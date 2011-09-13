5fe20e9ae0e6c702ba5a0c15d80f507f902cad12
Jacob-Kroeze <jlkroeze@gmail.com>
2011-09-13 11:25:59 -0700

    debug Mongo::Connection.from_uri

--- get-those-titles.rb
+++ get-those-titles.rb
@@ -17,12 +17,12 @@ puts ENV['MONGOLAB_URI']
 puts "*********************************************"
 puts URI.parse(ENV['MONGOLAB_URI']
 
-configure do
-  uri  = URI.parse(ENV['MONGOLAB_URI'])
-  conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
-  db = conn.db(uri.path.gsub(/^\//, ''))
-  Mongoid.database = db
-end
+#configure do
+#  uri  = URI.parse(ENV['MONGOLAB_URI'])
+#  conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
+#  db = conn.db(uri.path.gsub(/^\//, ''))
+#  Mongoid.database = db
+#end
 
 #configure :development do
 #  Mongoid.database = Mongo::Connection.new('localhost','27017').db('parse_emails')
